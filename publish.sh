00f1615bd2f5262b53e53ec7401218036c1c4278
Stephanie Rinaldi <srinaldi@arizona.edu>
2024-05-01 11:24:25 -0700

    Update publish.sh
    Removed MakeBeamer from publish as this script does not exist / MakeMarkdown is the one creating the Latex Beamers.

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# This makes markdown from HTML and the Latex Beamers
 make -f $SCRIPT_DIR/MakeMarkdown
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -19,20 +20,17 @@ else
     sed -i -e 's/{.*}//' dist/*.markdown
     sed -i -e 's/.html/.markdown/g' dist/*.markdown
 fi
+
 python $SCRIPT_DIR/RunGraphviz.py
 python $SCRIPT_DIR/MakeLinksGitHubFriendly.py
 
-#./guides/example_hook.sh
-
-#to make latex beamer slides, uncomment next three lines:
-make -f $SCRIPT_DIR/MakeBeamer
-
 #sed -i 's/{{find}}/{{replace}}/g' {{filename}}
+# Note- This doesn't appear to be working.
 sed -i ''  -e 's|L0.html\\\#||g' dist/*.tex
 sed -i ''  -e 's|L1.html\\\#||g' dist/*.tex
 sed -i ''  -e 's|L2.html\\\#||g' dist/*.tex
 sed -i ''  -e 's|L3.html\\\#||g' dist/*.tex
-sed -i ''  -e 's|L4-FOA-M2.html\\\#||g' dist/*.tex
+sed -i ''  -e 's|L4.html\\\#||g' dist/*.tex
 #fix internal links:
 sed -i ''  -e 's|href{L|hyperlink{L|g' dist/*.tex  # should not break weblinks so long as they don't start with L
 
